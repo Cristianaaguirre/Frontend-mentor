6bb007239ff385a344064c3858d833a9fefadf71
unknown <cristianaaguirre0@gmail.com>
2022-02-14 18:49:18 -0300

    terminando el diseño responsive

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -2,23 +2,23 @@ const button = document.querySelectorAll(".btn__container");
 const menu = document.querySelectorAll('.dropdown__item');
 
 for (let i = 0; i< button.length & menu.length; i++) {
-  const buttonOption = button[i]
-  const dropItem = menu[i]
-  buttonOption.addEventListener('click', openMenu) 
+  const buttonOption = button[i];
+  const dropItem = menu[i];
+  buttonOption.addEventListener('click', openMenu) ;
 
   function openMenu () {
       if (buttonOption.nextElementSibling.classList.contains('dropdown__item--open') === true) {
-        dropItem.classList.remove('dropdown__item--open') 
+        dropItem.classList.remove('dropdown__item--open');
       } else {
         cerrar()
-        dropItem.classList.add('dropdown__item--open') 
+        dropItem.classList.add('dropdown__item--open');
       }
   }
 }
 
 function cerrar () {
   menu.forEach(function cerrar(e) {
-    e.classList.remove('dropdown__item--open')
+    e.classList.remove('dropdown__item--open');
   })
 }
 
